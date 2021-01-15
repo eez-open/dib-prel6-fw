62b1c7e0d286bc1a1e7f79f6e73795ff391a77c8
Martin Vladic <martin.vladic@gmail.com>
2021-01-15 20:40:57 +0100

    refactoring

--- Core/Inc/utils.h
+++ Core/Inc/utils.h
@@ -1,3 +1,4 @@
 #define READ_PIN(PORT, PIN) ((PORT->IDR & PIN) ? 1 : 0)
 #define SET_PIN(PORT, PIN) PORT->BSRR = PIN
 #define RESET_PIN(PORT, PIN) PORT->BSRR = (uint32_t)PIN << 16
+#define WRITE_PIN(PORT, PIN, STATE) if (STATE) SET_PIN(PORT, PIN); else RESET_PIN(PORT, PIN)
--- Core/Src/firmware.cpp
+++ Core/Src/firmware.cpp
@@ -7,141 +7,114 @@
 #include "firmware.h"
 #include "utils.h"
 
-////////////////////////////////////////////////////////////////////////////////
+static const uint32_t CONF_SPI_TRANSFER_TIMEOUT_MS = 500;
+static const uint32_t CONF_RELAY_DEBOUNCE_TIME_MS = 10;
 
-SetParams currentState;
-
-////////////////////////////////////////////////////////////////////////////////
-
-static const int NUM_RELAYS = 6;
-
-static GPIO_TypeDef *RELAY_PORTS[NUM_RELAYS] = {
-	PR0_GPIO_Port, PR1_GPIO_Port, PR2_GPIO_Port,
-	PR3_GPIO_Port, PR4_GPIO_Port, PR5_GPIO_Port
-};
-
-static uint16_t RELAY_PINS[NUM_RELAYS] = {
-	PR0_Pin, PR1_Pin, PR2_Pin,
-	PR3_Pin, PR4_Pin, PR5_Pin
-};
-
-void Relays_Setup() {
-	for (int i = 0; i < NUM_RELAYS; i++) {
-		RESET_PIN(RELAY_PORTS[i], RELAY_PINS[i]);
-	}
-
-	HAL_Delay(10); // prevent debounce
-}
-
-void Relays_Update(SetParams &newState) {
-	for (int i = 0; i < NUM_RELAYS; i++) {
-		auto relayState = newState.relayStates & (1 << i);
-		if ((currentState.relayStates & (1 << i)) != relayState) {
-			if (relayState) {
-				SET_PIN(RELAY_PORTS[i], RELAY_PINS[i]);
-			} else {
-				RESET_PIN(RELAY_PORTS[i], RELAY_PINS[i]);
-			}
-		}
-	}
-
-	HAL_Delay(10); // prevent debounce
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// MASTER - SLAVE Communication
-
-extern SPI_HandleTypeDef hspi1;
+// master-slave communication
 extern "C" void SPI1_Init(void);
-SPI_HandleTypeDef *hspiMaster = &hspi1; // for MASTER-SLAVE communication
-
-#define BUFFER_SIZE 20
-
-uint32_t input[(BUFFER_SIZE + 3) / 4 + 1];
-uint32_t output[(BUFFER_SIZE + 3) / 4];
-volatile int transferCompleted;
-
-void beginTransfer() {
-    transferCompleted = 0;
-    HAL_SPI_TransmitReceive_DMA(hspiMaster, (uint8_t *)output, (uint8_t *)input, BUFFER_SIZE);
-    RESET_PIN(DIB_IRQ_GPIO_Port, DIB_IRQ_Pin);
-}
+extern SPI_HandleTypeDef hspi1;
+SPI_HandleTypeDef *hspiMaster = &hspi1; // SPI for MASTER-SLAVE communication
+volatile enum {
+	TRANSFER_STATE_WAIT,
+	TRANSFER_STATE_SUCCESS,
+	TRANSFER_STATE_ERROR
+} transferState;
 
 void HAL_SPI_TxRxCpltCallback(SPI_HandleTypeDef *hspi) {
-	if (hspi == hspiMaster) {
-		SET_PIN(DIB_IRQ_GPIO_Port, DIB_IRQ_Pin);
-		transferCompleted = 1;
-	}
+	SET_PIN(DIB_IRQ_GPIO_Port, DIB_IRQ_Pin);
+	transferState = TRANSFER_STATE_SUCCESS;
 }
 
 void HAL_SPI_ErrorCallback(SPI_HandleTypeDef *hspi) {
-	if (hspi == hspiMaster) {
-		SET_PIN(DIB_IRQ_GPIO_Port, DIB_IRQ_Pin);
-		transferCompleted = 2;
-	}
+	SET_PIN(DIB_IRQ_GPIO_Port, DIB_IRQ_Pin);
+	transferState = TRANSFER_STATE_ERROR;
 }
 
-int waitTransferCompletion() {
+// relays
+static const int NUM_RELAYS = 6;
+static GPIO_TypeDef *RELAY_PORTS[NUM_RELAYS] = { PR0_GPIO_Port, PR1_GPIO_Port, PR2_GPIO_Port, PR3_GPIO_Port, PR4_GPIO_Port, PR5_GPIO_Port };
+static uint16_t RELAY_PINS[NUM_RELAYS] = { PR0_Pin, PR1_Pin, PR2_Pin, PR3_Pin, PR4_Pin, PR5_Pin };
+uint8_t g_relayStates = 0; // use this to store the latest state of relays, at the beginning all are off
+
+// setup is called once at the beginning from the main.c
+extern "C" void setup() {
+	// setup relays, i. e. turn all off
+	for (int i = 0; i < NUM_RELAYS; i++) {
+		RESET_PIN(RELAY_PORTS[i], RELAY_PINS[i]);
+	}
+	HAL_Delay(CONF_RELAY_DEBOUNCE_TIME_MS); // prevent debounce
+}
+
+// loop is called, of course, inside the loop from the main.c
+extern "C" void loop() {
+	// start SPI transfer
+	static const size_t BUFFER_SIZE = 20;
+	uint32_t input[(BUFFER_SIZE + 3) / 4 + 1];
+	uint32_t output[(BUFFER_SIZE + 3) / 4];
+    transferState = TRANSFER_STATE_WAIT;
+    HAL_SPI_TransmitReceive_DMA(hspiMaster, (uint8_t *)output, (uint8_t *)input, BUFFER_SIZE);
+    RESET_PIN(DIB_IRQ_GPIO_Port, DIB_IRQ_Pin); // inform master that module is ready for the SPI communication
+
+    // wait for the transfer to finish
 	uint32_t startTick = HAL_GetTick();
-	while (!transferCompleted) {
-		if (HAL_GetTick() - startTick > 500) {
+	while (transferState == TRANSFER_STATE_WAIT) {
+		if (HAL_GetTick() - startTick > CONF_SPI_TRANSFER_TIMEOUT_MS) {
+			// transfer is taking too long to finish, maybe something is stuck, abort it
 			HAL_SPI_Abort(hspiMaster);
 			SET_PIN(DIB_IRQ_GPIO_Port, DIB_IRQ_Pin);
-			return 2;
+			transferState = TRANSFER_STATE_ERROR;
+			break;
 		}
 	}
-	return transferCompleted;
-}
 
-////////////////////////////////////////////////////////////////////////////////
-
-void Command_GetInfo(Request &request, Response &response) {
-	response.getInfo.firmwareMajorVersion = FIRMWARE_VERSION_MAJOR;
-	response.getInfo.firmwareMinorVersion = FIRMWARE_VERSION_MINOR;
-	response.getInfo.idw0 = HAL_GetUIDw0();
-	response.getInfo.idw1 = HAL_GetUIDw1();
-	response.getInfo.idw2 = HAL_GetUIDw2();
-}
-
-void Command_GetState(Request &request, Response &response) {
-	response.getState.tickCount = HAL_GetTick();
-}
-
-void Command_SetParams(Request &request, Response &response) {
-	Relays_Update(request.setParams);
-	memcpy(&currentState, &request.setParams, sizeof(SetParams));
-	response.setParams.result = 1; // success
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-extern "C" void setup() {
-	Relays_Setup();
-}
-
-extern "C" void loop() {
-	beginTransfer();
-    auto transferResult = waitTransferCompletion();
-
+	// Request and Response are defined in firmware.h
     Request &request = *(Request *)input;
     Response &response = *(Response *)output;
 
-    if (transferResult == 1) {
+    if (transferState == TRANSFER_STATE_SUCCESS) {
+    	// a way to tell the master that command was handled
     	response.command = 0x8000 | request.command;
 
 		if (request.command == COMMAND_GET_INFO) {
-			Command_GetInfo(request, response);
-		} else if (request.command == COMMAND_GET_STATE) {
-			Command_GetState(request, response);
-		} else if (request.command == COMMAND_SET_PARAMS) {
-			Command_SetParams(request, response);
-		} else {
+			// return back to the master firmware version and MCU id
+			response.getInfo.firmwareMajorVersion = FIRMWARE_VERSION_MAJOR;
+			response.getInfo.firmwareMinorVersion = FIRMWARE_VERSION_MINOR;
+			response.getInfo.idw0 = HAL_GetUIDw0();
+			response.getInfo.idw1 = HAL_GetUIDw1();
+			response.getInfo.idw2 = HAL_GetUIDw2();
+		}
+
+		else if (request.command == COMMAND_GET_STATE) {
+			// master periodically asks for the state of the module,
+			// for this module there is nothing much to return really
+			response.getState.tickCount = HAL_GetTick();
+		}
+
+		else if (request.command == COMMAND_SET_PARAMS) {
+			// turn on/off relays as instructed
+			for (int i = 0; i < NUM_RELAYS; i++) {
+				auto currentRelayState = g_relayStates & (1 << i);
+				auto newRelayState = request.setParams.relayStates & (1 << i);
+				if (currentRelayState != newRelayState) {
+					WRITE_PIN(RELAY_PORTS[i], RELAY_PINS[i], newRelayState);
+				}
+			}
+			g_relayStates = request.setParams.relayStates;
+			HAL_Delay(CONF_RELAY_DEBOUNCE_TIME_MS); // prevent debounce
+
+			response.setParams.result = 1; // success
+		}
+
+		else {
+			// unknown command received, tell the master that no command was handled
 			response.command = COMMAND_NONE;
 		}
     } else {
-		response.command = COMMAND_NONE;
-
+    	// invalid transfer, reinitialize SPI just in case
     	HAL_SPI_DeInit(hspiMaster);
 		SPI1_Init();
-    }
+
+		// tell the master that no command was handled
+		response.command = COMMAND_NONE;
+}
 }
